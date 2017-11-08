1b0dfdf2ba6e0abdfaea613c48ab1c87e72bae83
Matt Williams <matt@mtwilliams.uk>
2017-11-08 14:35:20 +0000

    Fixing relative call to actions

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import * as actions from '../actions/itemActions';
+import * as actions from '../../actions/itemActions';
 
 export const HomePage = (props) => {
   return (
