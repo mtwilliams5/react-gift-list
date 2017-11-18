1a205cb581da5789d34849d3e90630e3bf862751
Matt Williams <matt@mtwilliams.uk>
2017-11-18 22:22:09 +0000

    Adding a Header component for use on the lists pages

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {NavLink} from 'react-router-dom';
+
+const Header = ({requestors}) => {
+  return (
+    <ul className="nav nav-pills nav-justified">
+      {requestors.map(requestor =>
+        <li>
+          <NavLink exact to={'/' + requestor.id}>{requestor.firstName}</NavLink>
+        </li>
+      )}
+    </ul>
+  );
+}
+
+export default Header;
